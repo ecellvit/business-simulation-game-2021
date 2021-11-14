8c008bab3469c5876f4e450e8cab60010a927523
Vansh Chinda <vanshchinda@gmail.com>
2021-11-14 06:06:39 +0530

    Navbar added

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+/*Components*/
+import { Nav } from "../../components/nav";
+
+export function Landing() {
+  return (
+    <Box>
+      <Nav />
+    </Box>
+  );
+}
